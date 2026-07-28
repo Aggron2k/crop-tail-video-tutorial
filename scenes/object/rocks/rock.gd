bf446a4feae4687618ac80879820291b6abcd083
Horváth Krisztián <kriszcso4@gmail.com>
2026-07-28 22:15:33 +0200

    rock & stone + collect + shake

---
+++ scenes/object/rocks/rock.gd
@@ -0,0 +1,27 @@
+extends Sprite2D
+
+@onready var hurt_component: HurtComponent = $HurtComponent
+@onready var damage_component_node: DamageComponent = $DamageComponent
+
+var stone_scene = preload	("res://scenes/object/rocks/stone.tscn")
+
+func _ready() -> void:
+	hurt_component.hurt.connect(on_hurt)
+	damage_component_node.max_damaged_reached.connect(on_max_damage_reached)
+
+func on_hurt(hit_damage: int) -> void:
+	damage_component_node.apply_damage(hit_damage)
+	material.set_shader_parameter("shake_intensity", 0.3)
+	await get_tree().create_timer(0.5).timeout
+	material.set_shader_parameter("shake_intensity", 0.0)
+
+func on_max_damage_reached() -> void:
+	call_deferred("add_stone_scene")
+	print("max damage reached: stone")
+	queue_free()
+	
+func add_stone_scene() -> void:
+	var stone_instance = stone_scene.instantiate() as Node2D
+	stone_instance.global_position = global_position
+	get_parent().add_child(stone_instance)
+	
