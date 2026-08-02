4232a1e8ea171b7ad38425cbfa9d78908dc18c4b
Horváth Krisztián <kriszcso4@gmail.com>
2026-08-02 15:33:31 +0200

    Menu rendszer fix, focus a selected itemre

--- scenes/UI/tools_panel.gd
+++ scenes/UI/tools_panel.gd
@@ -6,6 +6,34 @@ extends PanelContainer
 @onready var tool_tomato: Button = $MarginContainer/HBoxContainer/ToolTomato
 
 
+func _ready() -> void:
+	ToolManager.tool_selected.connect(on_tool_selected)
+
+
+func on_tool_selected(tool: DataTypes.Tools) -> void:
+	match tool:
+		DataTypes.Tools.AxeWood:
+			tool_axe.grab_focus()
+		DataTypes.Tools.TillGround:
+			tool_tilling.grab_focus()
+		DataTypes.Tools.WaterCrops:
+			tool_watering_can.grab_focus()
+		DataTypes.Tools.PlantCorn:
+			tool_corn.grab_focus()
+		DataTypes.Tools.PlantTomato:
+			tool_tomato.grab_focus()
+		DataTypes.Tools.None:
+			release_all_focus()
+
+
+func release_all_focus() -> void:
+	tool_axe.release_focus()
+	tool_tilling.release_focus()
+	tool_watering_can.release_focus()
+	tool_corn.release_focus()
+	tool_tomato.release_focus()
+
+
 func _on_tool_axe_pressed() -> void:
 	ToolManager.select_tool(DataTypes.Tools.AxeWood)
 
@@ -29,8 +57,3 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT:
 			ToolManager.select_tool(DataTypes.Tools.None)
-			tool_axe.release_focus()
-			tool_tilling.release_focus()
-			tool_watering_can.release_focus()
-			tool_corn.release_focus()
-			tool_tomato.release_focus()
