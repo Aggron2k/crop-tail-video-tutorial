03a3e6f835ae4209d32552403fb80a42fa7a2d79
Horváth Krisztián <kriszcso4@gmail.com>
2026-08-07 19:01:06 +0200

    Collect items, Y Sorting, itemek lepakolása

--- scenes/components/collectable_component.gd
+++ scenes/components/collectable_component.gd
@@ -1,9 +1,10 @@
 class_name CollectableComponent
 extends Area2D
+
 @export var collecatble_name: String
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		print("Collected")
+		print("Collected:", collecatble_name)
 		get_parent().queue_free()
