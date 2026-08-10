4034391b064b9c293bdf673c1763e36d0eb5d763
Horváth Krisztián <kriszcso4@gmail.com>
2026-08-10 17:16:43 +0200

    Ültetés mehanizumus + Crop filed remove

--- scenes/components/crops_cursor_component.gd
+++ scenes/components/crops_cursor_component.gd
@@ -3,14 +3,14 @@ class_name CropsCursorComponent
 extends Node2D
 
 @export var tilled_soil_tilemap_layer: TileMapLayer
-@onready var player: Player = get_tree().get_first_node_in_Group("player")
+@export var player: Player
+@export var crop_fields: Node2D
 
-
-var corn_plant_scene = preload	("res://scenes/object/plants/corn.tscn")
-var tomato_plant_scene = preload	("res://scenes/object/plants/tomato.tscn")
+var corn_plant_scene = preload("res://scenes/object/plants/corn.tscn")
+var tomato_plant_scene = preload("res://scenes/object/plants/tomato.tscn")
 
 var mouse_position: Vector2
-var cell_positio: Vector2i
+var cell_position: Vector2i
 var cell_source_id: int
 var local_cell_position: Vector2
 var distance: float
@@ -18,25 +18,49 @@ var distance: float
 func _ready() -> void:
 	if !player:
 		player = get_tree().get_first_node_in_group("player") as Player
+	if !crop_fields:
+		crop_fields = get_parent().find_child("CropFields") as Node2D
+		if !crop_fields:
+			crop_fields = get_parent().find_child("CropField") as Node2D
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("remove_dirt"):
 		if ToolManager.selected_tool == DataTypes.Tools.TillGround:
 			get_cell_under_mouse()
-	elif event.is_action_pressed("hit"):
-		if ToolManager.selected_tool == DataTypes.Tools.TillGround:
+			remove_crop()
+			
+	if event.is_action_pressed("hit"):
+		if ToolManager.selected_tool == DataTypes.Tools.PlantCorn or ToolManager.selected_tool == DataTypes.Tools.PlantTomato:
 			get_cell_under_mouse()
+			add_crop()
 			
 func get_cell_under_mouse() -> void:
+	if !player:
+		player = get_tree().get_first_node_in_group("player") as Player
+		if !player:
+			return
+
 	mouse_position = tilled_soil_tilemap_layer.get_local_mouse_position()
 	cell_position = tilled_soil_tilemap_layer.local_to_map(mouse_position)
 	cell_source_id = tilled_soil_tilemap_layer.get_cell_source_id(cell_position)
-	local_cell_position = grass_tilled_soil_tilemap_layertilemap_layer.map_to_local(cell_position)
+	local_cell_position = tilled_soil_tilemap_layer.map_to_local(cell_position)
 	distance = player.global_position.distance_to(local_cell_position)
 
 func add_crop() -> void:
-	if distance < 20.0:
+	if distance < 20.0 and crop_fields:
 		if ToolManager.selected_tool == DataTypes.Tools.PlantCorn:
 			var corn_instance = corn_plant_scene.instantiate() as Node2D
 			corn_instance.global_position = local_cell_position
-			get_parent().find_child("CropField").add_child(corn_instance)
+			crop_fields.add_child(corn_instance)
+		elif ToolManager.selected_tool == DataTypes.Tools.PlantTomato:
+			var tomato_instance = tomato_plant_scene.instantiate() as Node2D
+			tomato_instance.global_position = local_cell_position
+			crop_fields.add_child(tomato_instance)
+			
+func remove_crop() -> void:
+	if distance < 20.0 and crop_fields:
+		var crop_nodes = crop_fields.get_children()
+		
+		for node: Node2D in crop_nodes:
+			if node.global_position == local_cell_position:
+				node.queue_free()
