0bac362e3696ed20ccc868e9175178470c0f464c
Horváth Krisztián <kriszcso4@gmail.com>
2026-08-11 11:27:08 +0200

    Game save + Game load pt.2

--- scenes/components/save_data_component.gd
+++ scenes/components/save_data_component.gd
@@ -8,7 +8,14 @@ extends Node
 func _ready() -> void:
 	add_to_group("save_data_component")
 
-func save_data() -> NodeDataResource:
-	if save_data_resource != null:
-		save_data_resource._save_data(parent_node)
+func save_data() -> Resource:
+	if parent_node == null:
+		return null
+		
+	if save_data_resource == null:
+		push_error("save_data_resource:", save_data_resource, parent_node.name)
+		return
+		
+	save_data_resource._save_data(parent_node)
+	
 	return save_data_resource
