0e87f473aa31458e5438a6b06fb07668f348a2b9
Horváth Krisztián <kriszcso4@gmail.com>
2026-08-11 11:36:44 +0200

    Game save + Game load pt.3

--- scenes/components/save_data_component.gd
+++ scenes/components/save_data_component.gd
@@ -8,13 +8,13 @@ extends Node
 func _ready() -> void:
 	add_to_group("save_data_component")
 
-func save_data() -> Resource:
+func save_data() -> NodeDataResource:
 	if parent_node == null:
 		return null
 		
 	if save_data_resource == null:
-		push_error("save_data_resource:", save_data_resource, parent_node.name)
-		return
+		push_error("save_data_resource is null on: ", parent_node.name)
+		return null
 		
 	save_data_resource._save_data(parent_node)
 	
