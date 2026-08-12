8db731db537c47245fbd42477ac526b07785af05
Horváth Krisztián <kriszcso4@gmail.com>
2026-08-12 09:36:08 +0200

    Inventory kezelés és animáció a ládába pakoláshoz

--- script/globals/inventory_manager.gd
+++ script/globals/inventory_manager.gd
@@ -12,3 +12,12 @@ func add_collectable(collectable_name: String) -> void:
 		inventory[collectable_name] += 1
 
 	inventory_change.emit()
+
+func remove_collectable(collactable_name: String) -> void:
+	if inventory[collactable_name] == null:
+		inventory[collactable_name] = 0
+	else:
+		if inventory[collactable_name] > 0:
+			inventory[collactable_name] -= 1
+	
+	inventory_change.emit()
